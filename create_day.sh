8761f31c4d423a8237fa60101f5731b053ffa640
Luse <github@damm.io>
2025-12-01 06:43:00 +0100

    done with cleanup

--- create_day.sh
+++ create_day.sh
@@ -47,6 +47,45 @@ func main() {
 }
 EOF
 
+cat > "$foldername/main_test.go" << 'EOF'
+package main
+
+import (
+	"testing"
+
+	"koltrast.net/AOC2025/utils"
+)
+
+
+func TestPart1(t *testing.T) {
+	input, err := utils.ReadLines("input_test.txt")
+	if err != nil {
+		t.Fatalf("Failed to read test input: %v", err)
+	}
+
+	result := part1(input)
+	expected := 0 // TODO: Add expected result
+
+	if result != expected {
+		t.Errorf("part1() = %d, want %d", result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input, err := utils.ReadLines("input_test.txt")
+	if err != nil {
+		t.Fatalf("Failed to read test input: %v", err)
+	}
+
+	result := part2(input)
+	expected := 0 // TODO: Add expected result
+
+	if result != expected {
+		t.Errorf("part2() = %d, want %d", result, expected)
+	}
+}
+EOF
+
 echo "Created $foldername/"
 echo "Don't forget to add your input.txt to $foldername/"
 echo "Run with: cd $foldername && go run main.go"
